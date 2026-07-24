7dba3febd1bc443b198624918449ee92287ba7f6
spazm82 <144231902+spazm82@users.noreply.github.com>
2026-07-24 14:03:51 +0500

    Update spazm82_site.py

--- spazm82_site.py
+++ spazm82_site.py
@@ -112,7 +112,7 @@ def go_streamlit():
 
         st.image("photo/7.jpg")
         st.write("Выходит 03 августа 2026. Но на сайте Вы можете послушать трек уже сейчас!")
-        yandex_disk_url_7 = "https://disk.yandex.ru/d/7CENZjqtT5MFPQ"
+        yandex_disk_url_7 = "https://disk.yandex.ru/d/2ek3R8L7ttYTOA"
         direct_audio_url_7 = f"https://getfile.dokpub.com/yandex/get/{yandex_disk_url_6}"
         st.audio(direct_audio_url_7, format="audio/mpeg")
 
